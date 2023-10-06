088b41fbee35b5fa23e6cc9a0e2b3e71674d5b8c
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2023-10-06 18:52:49 +0200

    change names

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar dense>
+
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+
+    <template v-slot:append>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      Forensic Med
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+//
 </script>
